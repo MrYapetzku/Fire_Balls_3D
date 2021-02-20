cc10c04ed229121efa20bebdb58389bfe47793a0
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-02-20 20:13:55 +0300

    Init

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _endLevelMassage;
+    [SerializeField] private Tower _tower;
+
+    private void OnEnable()
+    {
+        _tower.SizeUpdated += OnSizeUpdated;
+    }
+
+    private void OnDisable()
+    {
+        _tower.SizeUpdated -= OnSizeUpdated;
+    }
+
+    private void OnSizeUpdated(int size)
+    {
+        if (size == 0)
+        {
+            _endLevelMassage.text = "Tower is Broken!";
+        }
+    }
+}
